3ddac010960f3b9737e19273c0189967252778e9
GodCreator <theenigma@yandex.ru>
2017-12-04 02:57:13 +0300

    Init

---
+++ Assets/Scripts/DailyGiftController.cs
@@ -0,0 +1,244 @@
+﻿/*на основе скрипта команды инди-разработчиков Montana Games*/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using UnityEngine;
+
+public sealed class DailyGiftController : MonoBehaviour
+{
+    [Header("Settings")]
+    [Header("Others")]
+    private int DaysCount;
+    private const int MaxDayCount = 7;
+
+    private DateTime LastGiftDateTime;
+
+    private bool CanUserGetGift;
+
+    public Action<int> OnUserWantGift;
+
+    private List<DailyGiftItem> items;
+
+    public GameObject NoInternetMessage;
+
+    private string OnlineTimeUrl = "http://www.microsoft.com";
+
+    private string[] dayNames;
+    private string[] giftInfos;
+    private bool prVideo;//признак просмотра рекламы
+    private int purseOfPlayer;//аналог кошелька игрока
+    private void Start()
+    {
+
+    }
+    void Awake()
+    {
+        items = new List<DailyGiftItem>(transform.GetComponentsInChildren<DailyGiftItem>());
+
+        Start((days) =>
+        {
+            days++;  //первый день - 1        
+        },
+        MaxDayCount, // Максимальное количество дней подряд, которые видит пользователь
+        new[]
+        {
+            "1 День",
+            "2 День",
+            "3 День",
+            "4 День",
+            "5 День",
+            "6 День",
+            "7 День"
+        },
+        new[]
+        {
+            "10 GOlD",
+            "20 GOlD",
+            "30 GOlD",
+            "40 GOlD",
+            "50 GOlD",
+            "60 GOlD",
+            "Seif" //в котором 70 gold =)
+        }
+        );
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("DaysCount", DaysCount);
+        PlayerPrefs.SetString("LastGiftDateTime", LastGiftDateTime.ToLongDateString());
+    }
+    void Load()
+    {
+        DaysCount = PlayerPrefs.GetInt("DaysCount");
+
+        if (PlayerPrefs.HasKey("LastGiftDateTime"))
+            LastGiftDateTime = DateTime.Parse(PlayerPrefs.GetString("LastGiftDateTime"));
+    }
+
+
+    void SetInfos()
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            short status = 0;
+
+            if (DaysCount == i && CanUserGetGift)
+                status = 1;
+            else if (DaysCount > i)
+            {
+                status = 2;
+            }
+
+            var _dayName = string.Empty;
+
+            if (dayNames != null && dayNames.Length > i)
+            {
+                _dayName = dayNames[i];
+            }
+
+            var _giftInfo = string.Empty;
+
+            if (giftInfos != null && giftInfos.Length > i)
+            {
+                _giftInfo = giftInfos[i];
+            }
+
+            items[i].SetInfo(this, _dayName, _giftInfo, status);
+        }
+    }
+
+    void ShowNoInternetPanel()
+    {
+        if (NoInternetMessage != null)
+            NoInternetMessage.SetActive(true);
+    }
+
+    void HideNoInternetPanel()
+    {
+        if (NoInternetMessage != null)
+            NoInternetMessage.SetActive(false);
+    }
+    /// <summary>
+    /// Метод для запуска скрипта
+    /// </summary>
+    /// <param name="callback"> Функция в которую вернётся день (начиная с 0) за который пользовтелю нужно вручить подарок
+    /// По нажатию пользователем кнопки "Забрать подарок".
+    /// </param>
+    /// <param name="_MaxDaysCount">Максимальное количество дней подряд за которое пользователь получит подарок, и счёт сбросится на новый день</param>
+    /// <param name="dayNames">Массив для изменений названий дней</param>
+    /// <param name="giftInfos">Массив для изменения описания подарка</param>
+    public void Start(Action<int> callback, int _MaxDaysCount = MaxDayCount, string[] dayNames = null, string[] giftInfos = null)
+    {
+        Load();
+        this.dayNames = dayNames;
+        this.giftInfos = giftInfos;
+
+        if (callback != null)
+            OnUserWantGift = callback;
+
+        DateTime now = DateTime.Now;
+        now = GetNistTime();// Связь с сайтом microsoft
+        if (now == DateTime.MinValue)
+        {
+            ShowNoInternetPanel();
+            DaysCount = 0;
+            CanUserGetGift = false;
+            SetInfos();
+            return;
+        }
+        else
+            HideNoInternetPanel();
+
+        if (now.AddDays(-1).Day == LastGiftDateTime.Day &&
+            now.Hour > 3 &&//начало суток в 3.00
+            now.AddDays(-1).Month == LastGiftDateTime.Month &&
+            now.AddDays(-1).Year == LastGiftDateTime.Year)
+        {
+            CanUserGetGift = true;
+        }
+        else if (now.Day == LastGiftDateTime.Day &&
+             now.Month == LastGiftDateTime.Month && now.Year == LastGiftDateTime.Year)
+        {
+            CanUserGetGift = false;
+        }
+        else
+        if (now.Hour > 3)//начало суток в 3.00
+        {//сброс цикла дней на начало (если не заходили один день)
+            DaysCount = 0;
+            CanUserGetGift = true;
+        }
+        SetInfos();
+    }
+
+    internal void GetGift()
+    {
+        if (CanUserGetGift)
+        {
+            OnUserWantGift(DaysCount);
+            DaysCount++;
+
+            if (DaysCount == MaxDayCount)
+                DaysCount = 0;
+
+            LastGiftDateTime = DateTime.Now;
+            CanUserGetGift = false;
+        }
+        Save();
+        SetInfos();
+        GetGiftInProject(); //получение подарка
+    }
+    private DateTime GetNistTime()
+    {
+        var myHttpWebRequest = (HttpWebRequest)WebRequest.Create(OnlineTimeUrl);
+        var response = myHttpWebRequest.GetResponse();
+        string todaysDates = response.Headers["date"];
+        return System.DateTime.ParseExact(todaysDates, "ddd, dd MMM yyyy HH:mm:ss 'GMT'", CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.AssumeUniversal);
+    }
+    public void ResetInfo()
+    {
+        PlayerPrefs.DeleteAll();	// очистка всей информации для этого приложения
+    }
+    public void SeeVideo()
+    {
+        prVideo = true;//просмотрена реклама
+    }
+    public void GetGiftInProject() //метод выдачи подарка. Так же его содержимое можно вызывать в любом месте проекта
+    {
+        int _gift = 10; //стартовая сумма вознаграждения
+        if (prVideo)//если был просмотрен видео-ролик
+            _gift *= 2;//подарок удваиваем
+        prVideo = false;
+        FindObjectOfType<DailyGiftController>().Start((day) =>
+        {
+            switch (day)
+            {
+                case 1://Первый день
+                    purseOfPlayer += _gift * 1;//Выдаём подарок пользователю.
+                    break;
+                case 2://Второй день
+                    purseOfPlayer += _gift * 2;
+                    break;
+                case 3://Третий день 
+                    purseOfPlayer += _gift * 3;
+                    break;
+                case 4://Четвертый день
+                    purseOfPlayer += _gift * 4;
+                    break;
+                case 5://Пятый день
+                    purseOfPlayer += _gift * 5;
+                    break;
+                case 6://Шестой день 
+                    purseOfPlayer += _gift * 6;
+                    break;
+                case 7://Седьмой день 
+                    purseOfPlayer += _gift * 7;
+                    break;
+            }
+        },
+        7 //Максимальное количество дней подряд
+        );
+        PlayerPrefs.SetInt("purseOfPlayer", purseOfPlayer);//сохраняем сумму в кошельке
+    }
+}
---
+++ Assets/Scripts/DailyGiftItem.cs
@@ -0,0 +1,104 @@
+﻿/*на основе скрипта команды инди-разработчиков Montana Games*/
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class DailyGiftItem : MonoBehaviour
+{
+    private DailyGiftController controller;
+    void OnEnable() //получаем ссылки на элементы Item для связи
+    {
+        if (day == null)
+        {
+            var o1 = transform.Find("day");
+            if (o1 != null) day = o1.GetComponent<Text>();
+        }
+
+        if (giftInfo == null)
+        {
+            var gameObject1 = transform.Find("GiftInfo");
+            if (gameObject1 != null)
+                giftInfo = gameObject1.GetComponent<Text>();
+        }
+
+        if (GiftedPanel == null)
+        {
+            var panel = transform.Find("GiftedPanel");
+            if (panel != null)
+                GiftedPanel = panel.gameObject;
+        }
+
+        if (GiftedText == null)
+        {
+            var text = transform.Find("GiftedText");
+            if (text != null) GiftedText = text.gameObject;
+        }
+
+        if (GetGiftButton == null)
+        {
+            var giftButton = transform.Find("GetGiftButton");
+            if (giftButton != null)
+                GetGiftButton = giftButton.gameObject;
+        }
+        if (GetVideoButton == null)
+        {
+            var VideoButton = transform.Find("GetVideoButton");
+            if (VideoButton != null)
+                GetVideoButton = VideoButton.gameObject;
+        }
+        if (LockedPanel == null)
+        {
+            var o = transform.Find("LockedPanel");
+            if (o != null)
+                LockedPanel = o.gameObject;
+        }
+    }
+    private void Start()
+    {//если не доступна одна из кнопок получения подарка - отключаем анимацию окна
+        GetComponent<Animation>().enabled = GetVideoButton.activeInHierarchy;
+    }
+    public Text day;
+    public Text giftInfo;
+    public GameObject GiftedPanel;
+    public GameObject GiftedText;
+    public GameObject GetGiftButton;
+    public GameObject GetVideoButton;
+    public GameObject LockedPanel;
+
+    public void SetInfo(DailyGiftController controller, string dayText, string giftInfo, int status = 0)
+    {
+        this.controller = controller;
+        this.day.text = dayText;
+        this.giftInfo.text = giftInfo;
+
+        switch (status)
+        {
+            case 0:
+                StatusWindow(false, false, false, false, true);//default status (статус окна "награда не доступна"(кнопки скрыты, затемнения нет))
+                break;
+            case 1:
+                StatusWindow(false, false, true, true, false);// Gift can givet (статус окна "награда доступна, ожидает получения")
+                break;
+            case 2:
+                StatusWindow(true, true, false, false, false);// Gift collected (статус окна "награда получена"(кнопка скрыта, затемнение есть))
+                break;
+        }
+    }
+
+    public void OnClickGetGift()
+    {
+        GetComponent<Animation>().enabled = false;//отключаем анимацию окна
+        controller.GetGift();
+    }
+
+    void StatusWindow(bool _GiftedPanel, bool _text, bool _buttonGift, bool _buttonVideo, bool _LockedPanel) //default status 
+    {
+        if (GiftedPanel != null) GiftedPanel.SetActive(_GiftedPanel);
+        if (GiftedText != null) GiftedText.SetActive(_text);
+        if (GetGiftButton != null) GetGiftButton.SetActive(_buttonGift);
+        if (GetVideoButton != null) GetVideoButton.SetActive(_buttonVideo);
+        if (LockedPanel != null) LockedPanel.SetActive(_LockedPanel);
+
+    }
+}
